c2dec2b7b36f5af8e4b2dc4e78cf3cee7f37847b
unknown <emranulfarhad@gmail.com>
2022-04-17 12:06:20 +0600

    herro responsive done

--- src/components/HerroPart/Herro.js
+++ src/components/HerroPart/Herro.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Blink from '../../blinkanimation/blink';
+import Home from '../../blinkanimation/blink';
+import man from '../../Images/man2.png'
 import Nav from '../Navbar/Nav';
 import './Herro.css'
 
@@ -6,9 +9,15 @@ const Herro = () => {
     return (
         <div>
             <div className='herrobg'>
-                <Nav></Nav>
-
-                
+                <Nav ></Nav>
+                <div className=" container heropart">
+                <div>
+                    <Blink></Blink>
+                    </div>
+                    <div className="heroimg d-sm-none d-md-block d-lg-block d-xl-block ">
+                        <img className='img-fluid d-sm-none d-md-block d-lg-block d-xl-block' src={man} alt="" />
+                    </div>
+                </div>       
             </div>
             <div>
 
