883732c9a5dfcfdc2a58525799d792fd80ebf2ae
unknown <emranulfarhad@gmail.com>
2022-04-18 20:00:54 +0600

    title fav readme edit done

--- src/components/Aboutme/About.js
+++ src/components/Aboutme/About.js
@@ -27,7 +27,7 @@ const About = () => {
                         {/* <p>Hello! My name is Kedar, rhymes with cheddar, and I'm a <strong>photographer</strong> and <strong>front end dev</strong> intern at Dirigible Studio here in Madison, Wisco.
                             <p /> */}
 
-                            <p> My name Is <strong>Emranul Farhad </strong>  A webdeveloper & photographer , I have 4 years  plus experince in photography. But as a wevdeveloper I satrt my carera 5moths ago . <strong> HTML,CSS , BOOTSTRAP5, TAILWIND,REACT MUI, RACT FIREBASE HOOKS, FIREBASE AUTHENTICATION   JAVASCIPT, REACT, In this path i am beginar but i will doing lots of hard work for learning programing language for became a wevdeveloper.   </strong> </p>
+                            <p> My name Is <strong>Emranul Farhad </strong>  A webdeveloper & photographer , I have 4 years  plus experince in photography. But as a wevdeveloper I satrt my carera 5moths ago .  HTML,CSS , BOOTSTRAP5, TAILWIND,REACT MUI, RACT FIREBASE HOOKS, FIREBASE AUTHENTICATION   JAVASCIPT, REACT, In this path i am beginar but i will doing lots of hard work for learning programing language for became a wevdeveloper.    </p>
 
                             <p> Next 5 monts my plane is I will work harder so that I can achieve the goal for my job in the next 2 months as well i will work hard to learn backend technolgy such as node.js mongo db etc  </p>
                     </div>
